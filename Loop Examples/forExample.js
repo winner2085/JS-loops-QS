6251442e7a9c9d637a75ea3f3869cda99fc41f88
Erica <ericac2700@tuckahoeschools.org>
2024-12-03 13:26:22 +0000

    initial commit

--- Loop Examples/forExample.js
+++ Loop Examples/forExample.js
@@ -1,1 +1,8 @@
+let outputDiv = document.getElementById("output");
+outputDiv.innerHTML += "<h2>Using a 'for' loop to create a star pattern.</h2>";
 
+let pyramid = "";
+for(let i = 1; i < 10; i++){
+    pyramid += "*".repeat(i) + "<br>";
+}
+outputDiv.innerHTML += pyramid;
